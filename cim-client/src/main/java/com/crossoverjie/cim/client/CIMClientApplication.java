2a6b4f5f696572d2b8faf61280eb6010e06cf863
wayaya <dongsibei@ofworld.com>
2020-04-11 09:05:19 +0800

    打印日志

--- cim-client/src/main/java/com/crossoverjie/cim/client/CIMClientApplication.java
+++ cim-client/src/main/java/com/crossoverjie/cim/client/CIMClientApplication.java
@@ -13,23 +13,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author crossoverJie
  */
 @SpringBootApplication
-public class CIMClientApplication implements CommandLineRunner{
+public class CIMClientApplication implements CommandLineRunner {
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(CIMClientApplication.class);
+    private final static Logger logger = LoggerFactory.getLogger(CIMClientApplication.class);
 
-	@Autowired
-	private ClientInfo clientInfo ;
-	public static void main(String[] args) {
+    private final ClientInfo clientInfo;
+
+    public CIMClientApplication(ClientInfo clientInfo) {
+        logger.info("应用初始化 ...");
+        this.clientInfo = clientInfo;
+    }
+
+    public static void main(String[] args) {
+        logger.info("main start");
         SpringApplication.run(CIMClientApplication.class, args);
-		LOGGER.info("启动 Client 服务成功");
-	}
+        logger.info("启动 Client 服务成功");
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Scan scan = new Scan() ;
-		Thread thread = new Thread(scan);
-		thread.setName("scan-thread");
-		thread.start();
-		clientInfo.saveStartDate();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("守护进程...");
+        Scan scan = new Scan();
+        Thread thread = new Thread(scan);
+        thread.setName("scan-thread");
+        thread.start();
+        clientInfo.saveStartDate();
+    }
 }
